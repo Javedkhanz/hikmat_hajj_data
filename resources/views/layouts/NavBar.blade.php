c267026e9cf46543d28e45f41445ca01bd9e9d97
Javedkhanz <javed49818@gmail.com>
2023-09-02 02:31:49 -0700

    your message

---
+++ resources/views/layouts/NavBar.blade.php
@@ -0,0 +1,11 @@
+<div class="flex  md:text-xl text-xs space-x-4 justify-end px-4 bg-white border-b border-gray-100">
+    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        Add Haji
+    </button>
+    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Defaulter Haji
+    </button>
+    <button class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+        List of Incomplete hajji Data
+    </button>
+</div>
